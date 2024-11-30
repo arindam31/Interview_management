9b160c2c3233e910bac6bb52d249e7968e61864a
arindam31 <thearc31@gmail.com>
2024-11-30 03:13:59 +0100

    First test pass

--- imt/tests/users/test_views.py
+++ imt/tests/users/test_views.py
@@ -3,11 +3,14 @@ from django.test import TestCase
 from users.models import Staff
 from users.views import StaffView
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
 class UserViewTest(TestCase):
     def setUp(self):
-        self.staff = Staff.objects.create(first_name="S1")
+        user = get_user_model()
+        self.user = user.objects.create(username="TestUser", password="Bogus$321")
+        self.staff = Staff.objects.create(first_name="S1", user=self.user)
 
     def test_staff(self):
-        pass
+        self.assertEqual(1, 1)
